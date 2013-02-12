950d0532d2a7fa53ec2d329ff670f3cffe54c087
Drew DeVault <sir@cmpwn.com>
2013-02-12 00:09:18 -0700

    Added missing file; made column width consistent on peer list

---
+++ Patchy/Converters/PeerClientConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using MonoTorrent.Common;
+
+namespace Patchy.Converters
+{
+    public class PeerClientConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var client = (Software)value;
+            return client.Client.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
