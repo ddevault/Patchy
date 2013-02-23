30d867613cb43a91c1f0631733278018da805b9d
Drew DeVault <sir@cmpwn.com>
2013-02-23 12:06:14 -0700

    Added settings for individual torrents; closes #42

---
+++ Patchy/Converters/NetworkSpeedConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Patchy.Converters
+{
+    public class NetworkSpeedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (int)value / 1000;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (int)value * 1000;
+        }
+    }
+}
