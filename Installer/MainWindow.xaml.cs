6c9a9c3cbe9e048ea92776f1d56546a2f993e5d1
Drew DeVault <sir@cmpwn.com>
2013-02-24 14:29:05 -0700

    Added install path selection to installer

--- Installer/MainWindow.xaml.cs
+++ Installer/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Installer
 {
@@ -27,12 +26,66 @@ namespace Installer
             var reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("Installer.LICENSE"));
             licenseText.Text = reader.ReadToEnd();
             reader.Close();
+            installPathTextBox.Text = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "Patchy");
             Loaded += MainWindow_Loaded;
         }
 
         void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             
+        }
+
+        private void previousButtonClick(object sender, RoutedEventArgs e)
+        {
+            progressTabs.SelectedIndex--;
+            if (progressTabs.SelectedIndex != progressTabs.Items.Count - 1)
+                nextButton.Content = "Next";
+            previousButton.IsEnabled = (progressTabs.SelectedIndex != 0);
+        }
+
+        private void nextButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (progressTabs.SelectedIndex == 1) // Install path
+            {
+                if (!Directory.Exists(installPathTextBox.Text))
+                {
+                    var result = MessageBox.Show("The specified directory does not exist. Create it?", "Error", MessageBoxButton.YesNo, MessageBoxImage.Error);
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        try
+                        {
+                            Directory.CreateDirectory(installPathTextBox.Text);
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Unable to create directory.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                            return;
+                        }
+                    }
+                    else
+                        return;
+                }
+            }
+            if (progressTabs.SelectedIndex == progressTabs.Items.Count - 1)
+            {
+                // "Finish"
+            }
+            progressTabs.SelectedIndex++;
+            if (progressTabs.SelectedIndex == progressTabs.Items.Count - 1)
+                nextButton.Content = "Finish";
+            previousButton.IsEnabled = (progressTabs.SelectedIndex != 0);
+        }
+
+        private void browseSourceClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/SirCmpwn/Patchy");
+        }
+
+        private void browseInstallLocationClick(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                installPathTextBox.Text = dialog.SelectedPath;
         }
     }
 }
