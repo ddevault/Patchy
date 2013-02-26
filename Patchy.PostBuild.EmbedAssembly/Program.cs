38f3556415f006ed14c07f8ce780e10a16516b68
Drew DeVault <sir@cmpwn.com>
2013-02-26 11:11:07 -0700

    Finished post build tool to embed resources

--- Patchy.PostBuild.EmbedAssembly/Program.cs
+++ Patchy.PostBuild.EmbedAssembly/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Mono.Cecil;
+using System.IO;
 
 namespace Patchy.PostBuild.EmbedAssembly
 {
@@ -11,6 +13,16 @@ namespace Patchy.PostBuild.EmbedAssembly
         static void Main(string[] args)
         {
             // Usage: Patchy.PostBuild.EmbedAssembly <target assembly> <assemblies...>
+            AssemblyDefinition target;
+            using (var stream = File.OpenRead(args[0]))
+                target = AssemblyDefinition.ReadAssembly(stream);
+            for (int i = 1; i < args.Length; i++)
+            {
+                var data = File.ReadAllBytes(args[i]);
+                target.MainModule.Resources.Add(new EmbeddedResource(Path.GetFileName(args[i]), ManifestResourceAttributes.Public, data));
+            }
+            using (var stream = File.Create(args[0]))
+                target.Write(stream);
         }
     }
 }
