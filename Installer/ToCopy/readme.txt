64c3220b7f41bce9a95881e90a0a55ba92dda37b
Drew DeVault <sir@cmpwn.com>
2013-02-26 11:08:10 -0700

    Started on reworking of embedded resources in installer

---
+++ Installer/ToCopy/readme.txt
@@ -0,0 +1,3 @@
+﻿This directory contains all the assemblies to be embedded as links. This way, every time you compile,
+the files are updated with all the dependencies before compiling the installer. In the post-build, 
+some of these are compressed and copied into the binary.
---
+++ Patchy.PostBuild.EmbedAssembly/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Patchy.PostBuild.EmbedAssembly
+{
+    // Uses Mono.Cecil to embed assemblies into the installer
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Usage: Patchy.PostBuild.EmbedAssembly <target assembly> <assemblies...>
+        }
+    }
+}
