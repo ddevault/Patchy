857590afc6872481bada73a9948704f11151cf5f
Drew DeVault <sir@cmpwn.com>
2013-04-29 16:32:24 -0600

    Bumped version number

--- Patchy/MainWindow.Logic.cs
+++ Patchy/MainWindow.Logic.cs
@@ -28,7 +28,7 @@ namespace Patchy
 {
     public partial class MainWindow
     {
-        private const int CurrentVersion = 2; // Increment every time we push an update
+        private const int CurrentVersion = 3; // Increment every time we push an update
 
         private ClientManager Client { get; set; }
         private Timer Timer { get; set; }
